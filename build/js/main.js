f36c7de18b557f1799c8c35011be40cf6f4ee8b1
EjikeOdoh <ejike.developer@gmail.com>
2025-01-07 12:07:09 +0100

    Basic types in typescript

--- build/js/main.js
+++ build/js/main.js
@@ -1,12 +1,53 @@
 "use strict";
-let isLoading;
-let album;
-album = 56;
-album = "odogwu";
-function sum(a, b) {
-    return a + b;
-}
-const add = (a, b) => {
-    return a + b;
+let stringArr = ["One", "Hi", "Helo"];
+let guitars = ["Bass", "Tenor", 1209];
+let mixedData = ["Tesla", 1232, true];
+stringArr[0] = "Hi";
+stringArr.push("68");
+guitars[0] = "Hello";
+guitars.unshift(1001);
+mixedData = guitars;
+let test = [];
+let bands = [];
+bands.push("Westlife");
+let myTuple;
+myTuple = ["Emma", 23, true];
+bands[3] = "Hello";
+let myObj;
+myObj = [];
+let exampleObj = {
+    prop1: "Hello",
+    prop2: true
 };
-let reg = /\w+/g;
+exampleObj.prop2 = false;
+// interface Guitarist {
+//     name: string,
+//     active?: boolean,
+//     albums: (string | number)[]
+// }
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1144]
+};
+let jp = {
+    name: 'Jimmy',
+    albums: ["I", "IV"]
+};
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toLowerCase()}`;
+    }
+    return 'Hello';
+};
+console.log(greetGuitarist(jp));
+var Grade;
+(function (Grade) {
+    Grade[Grade["A"] = 1] = "A";
+    Grade[Grade["B"] = 2] = "B";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["D"] = 4] = "D";
+    Grade[Grade["E"] = 5] = "E";
+    Grade[Grade["F"] = 6] = "F";
+})(Grade || (Grade = {}));
+console.log(Grade[1]);
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,77 @@
-let isLoading: boolean;
+let stringArr = ["One", "Hi", "Helo"]
 
-let album: string | number;
+let guitars = ["Bass", "Tenor", 1209]
 
-album = 56;
-album = "odogwu"
+let mixedData = ["Tesla", 1232, true]
 
-function sum(a: number, b: number): number {
-    return a + b
+stringArr[0] = "Hi"
+stringArr.push("68")
+
+guitars[0] = "Hello"
+guitars.unshift(1001)
+
+mixedData = guitars
+
+let test = []
+let bands: string[] = [];
+
+bands.push("Westlife")
+
+let myTuple: [string, number, boolean]
+myTuple = ["Emma", 23, true]
+
+bands[3] = "Hello"
+
+let myObj: object;
+myObj = []
+
+let exampleObj = {
+    prop1: "Hello",
+    prop2: true
 }
 
-const add = (a: number, b: string): string => {
-    return a + b
+exampleObj.prop2 = false;
+
+type Guitarist = {
+    name?: string,
+    active?: boolean,
+    albums: (string | number)[]
 }
 
-let reg: RegExp = /\w+/g
+// interface Guitarist {
+//     name: string,
+//     active?: boolean,
+//     albums: (string | number)[]
+// }
+
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1144]
+}
+
+let jp: Guitarist = {
+    name: 'Jimmy',
+    albums: ["I", "IV"]
+}
+
+
+const greetGuitarist = (guitarist: Guitarist) => {
+    if (guitarist.name) {
+        return `Hello ${guitarist.name.toLowerCase()}`
+    }
+    return 'Hello'
+}
+
+console.log(greetGuitarist(jp))
+
+enum Grade {
+    A = 1,
+    B,
+    C,
+    D,
+    E,
+    F
+}
+
+console.log(Grade[1])
