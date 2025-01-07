53dad18aee5bf338895e367594a50bfaf99b31eb
EjikeOdoh <ejike.developer@gmail.com>
2025-01-07 09:27:01 +0100

    First commit

---
+++ build/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+let isLoading;
+let album;
+album = 56;
+album = "odogwu";
+function sum(a, b) {
+    return a + b;
+}
+const add = (a, b) => {
+    return a + b;
+};
+let reg = /\w+/g;
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+let isLoading: boolean;
+
+let album: string | number;
+
+album = 56;
+album = "odogwu"
+
+function sum(a: number, b: number): number {
+    return a + b
+}
+
+const add = (a: number, b: string): string => {
+    return a + b
+}
+
+let reg: RegExp = /\w+/g
