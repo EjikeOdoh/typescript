334acc78a78a7c0860c1434590f4c52174e707d0
EjikeOdoh <ejike.developer@gmail.com>
2025-01-10 00:40:03 +0100

    End of custom types

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,9 @@
 "use strict";
+let jp = {
+    name: "Jimmy",
+    active: true,
+    albums: [122, "Hi"]
+};
 let userName;
 userName = "K";
 console.log(userName);
@@ -10,3 +15,54 @@ const logMsg = (msg) => {
 };
 logMsg(23);
 logMsg(add(2, 3));
+let multiply = function (a, b) {
+    return a * b;
+};
+logMsg(multiply(3, 4));
+const addAll = (a, b, c) => {
+    if (typeof c !== 'undefined') {
+        return a + b + c;
+    }
+    return a + b;
+};
+logMsg(addAll(1, 2, 3));
+const sumAll = (a = 5, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(sumAll(undefined, 2));
+// Rest Parameters
+const total = (...nums) => {
+    let sum = 0;
+    nums.forEach(element => {
+        sum += element;
+    });
+    return sum;
+};
+logMsg(total(1, 2, 3));
+// Never type
+const createError = (msg) => {
+    throw new Error(msg);
+};
+let infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+// custom type guard
+function isNumber(value) {
+    return typeof value === 'number'
+        ? true : false;
+}
+// Use of never type
+function printType(value) {
+    if (typeof value === "string")
+        return 'String';
+    if (isNumber(value))
+        return 'Number';
+    return createError('This should never happen');
+}
+logMsg(isNumber('2'));
+logMsg(printType('two'));
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ type stringOrNumberArray = (string | number)[]
 type Guitarist = {
     name: string,
     active: boolean,
-    albums: stringOrNumberArray
+    albums: stringOrNumber[]
 }
+
+let jp: Guitarist = {
+    name: "Jimmy",
+    active: true,
+    albums: [122, "Hi"]
+}
+
 
 let userName: 'E' | 'J' | 'K'
 
@@ -24,4 +31,73 @@ const logMsg = (msg: any): void => {
 }
 
 logMsg(23)
-logMsg(add(2,3))
+logMsg(add(2, 3))
+
+type mathFunction = (a: number, b: number) => number
+
+let multiply: mathFunction = function (a, b) {
+    return a * b
+}
+
+logMsg(multiply(3, 4))
+
+const addAll = (a: number, b: number, c?: number): number => {
+    if (typeof c !== 'undefined') {
+
+        return a + b + c
+    }
+    return a + b
+}
+
+logMsg(addAll(1, 2, 3))
+
+const sumAll = (a: number = 5, b: number, c: number = 2): number => {
+    return a + b + c
+}
+
+logMsg(sumAll(undefined, 2))
+
+// Rest Parameters
+
+const total = (...nums: number[]): number => {
+    let sum: number = 0
+    nums.forEach(element => {
+        sum += element
+    });
+    return sum
+}
+
+
+logMsg(total(1, 2, 3))
+
+
+// Never type
+const createError = (msg: string) => {
+    throw new Error(msg)
+}
+
+let infinite = () => {
+    let i: number = 1
+    while (true) {
+        i++
+        if (i > 100) break
+    }
+}
+
+
+// custom type guard
+function isNumber (value: any):boolean {
+return typeof value === 'number' 
+    ? true : false
+}
+
+// Use of never type
+function printType(value: number | string): string {
+    if (typeof value === "string") return 'String'
+    if (isNumber(value)) return 'Number'
+    return createError('This should never happen')
+}
+
+logMsg(isNumber('2'))
+logMsg(printType('two'))
+
