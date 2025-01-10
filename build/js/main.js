4bba1dd1d7ed775eeff6f4c1d3e4f6c719d1893e
EjikeOdoh <ejike.developer@gmail.com>
2025-01-10 22:17:22 +0100

    Type assertion

--- build/js/main.js
+++ build/js/main.js
@@ -1,68 +1,20 @@
 "use strict";
-let jp = {
-    name: "Jimmy",
-    active: true,
-    albums: [122, "Hi"]
-};
-let userName;
-userName = "K";
-console.log(userName);
-const add = (a, b) => {
-    return a + b;
-};
-const logMsg = (msg) => {
-    console.log(msg);
-};
-logMsg(23);
-logMsg(add(2, 3));
-let multiply = function (a, b) {
-    return a * b;
-};
-logMsg(multiply(3, 4));
-const addAll = (a, b, c) => {
-    if (typeof c !== 'undefined') {
-        return a + b + c;
+// let a:One = 'hello'
+// let b = a as Two
+// let c = a as Three
+let a = "world";
+function addOrConcat(a, b, c) {
+    if (c === "add") {
+        return a + b;
     }
-    return a + b;
-};
-logMsg(addAll(1, 2, 3));
-const sumAll = (a = 5, b, c = 2) => {
-    return a + b + c;
-};
-logMsg(sumAll(undefined, 2));
-// Rest Parameters
-const total = (...nums) => {
-    let sum = 0;
-    nums.forEach(element => {
-        sum += element;
-    });
-    return sum;
-};
-logMsg(total(1, 2, 3));
-// Never type
-const createError = (msg) => {
-    throw new Error(msg);
-};
-let infinite = () => {
-    let i = 1;
-    while (true) {
-        i++;
-        if (i > 100)
-            break;
-    }
-};
-// custom type guard
-function isNumber(value) {
-    return typeof value === 'number'
-        ? true : false;
+    return "" + a + b;
 }
-// Use of never type
-function printType(value) {
-    if (typeof value === "string")
-        return 'String';
-    if (isNumber(value))
-        return 'Number';
-    return createError('This should never happen');
-}
-logMsg(isNumber('2'));
-logMsg(printType('two'));
+let myVal = addOrConcat(2, 4, 'concat');
+let nextVal = addOrConcat(2, 4, 'concat');
+console.log(nextVal);
+// The DOM
+const img = document.querySelector('img');
+const myImg = document.getElementById('img');
+const nextImg = document.getElementById('img');
+img.src;
+myImg.src;
--- src/main.ts
+++ src/main.ts
@@ -1,103 +1,31 @@
+type One = string
+type Two = string|number
+type Three = 'hello'
 
-type stringOrNumber = string | number
-
-type stringOrNumberArray = (string | number)[]
-
-type Guitarist = {
-    name: string,
-    active: boolean,
-    albums: stringOrNumber[]
-}
-
-let jp: Guitarist = {
-    name: "Jimmy",
-    active: true,
-    albums: [122, "Hi"]
-}
+// let a:One = 'hello'
+// let b = a as Two
+// let c = a as Three
 
 
-let userName: 'E' | 'J' | 'K'
+let a = <One>"world"
 
-userName = "K"
-
-console.log(userName)
-
-const add = (a: number, b: number): number => {
-    return a + b
-}
-
-const logMsg = (msg: any): void => {
-    console.log(msg)
-}
-
-logMsg(23)
-logMsg(add(2, 3))
-
-type mathFunction = (a: number, b: number) => number
-
-let multiply: mathFunction = function (a, b) {
-    return a * b
-}
-
-logMsg(multiply(3, 4))
-
-const addAll = (a: number, b: number, c?: number): number => {
-    if (typeof c !== 'undefined') {
-
-        return a + b + c
+function addOrConcat(a:number, b:number, c: 'add' | "concat"): string | number {
+    if (c === "add") {
+        return a+b
     }
-    return a + b
+    return ""+a+b
 }
 
-logMsg(addAll(1, 2, 3))
+let myVal:string = addOrConcat(2,4,'concat') as string
+let nextVal:number = addOrConcat(2,4,'concat') as number
 
-const sumAll = (a: number = 5, b: number, c: number = 2): number => {
-    return a + b + c
-}
+console.log(nextVal)
 
-logMsg(sumAll(undefined, 2))
+// The DOM
 
-// Rest Parameters
+const img = document.querySelector('img')! 
+const myImg = document.getElementById('img') as HTMLImageElement
+const nextImg = <HTMLImageElement>document.getElementById('img')
 
-const total = (...nums: number[]): number => {
-    let sum: number = 0
-    nums.forEach(element => {
-        sum += element
-    });
-    return sum
-}
-
-
-logMsg(total(1, 2, 3))
-
-
-// Never type
-const createError = (msg: string) => {
-    throw new Error(msg)
-}
-
-let infinite = () => {
-    let i: number = 1
-    while (true) {
-        i++
-        if (i > 100) break
-    }
-}
-
-
-// custom type guard
-function isNumber (value: any):boolean {
-return typeof value === 'number' 
-    ? true : false
-}
-
-// Use of never type
-function printType(value: number | string): string {
-    if (typeof value === "string") return 'String'
-    if (isNumber(value)) return 'Number'
-    return createError('This should never happen')
-}
-
-logMsg(isNumber('2'))
-logMsg(printType('two'))
-
+img.src
+myImg.src
